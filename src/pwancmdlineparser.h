242705adf5b05876ee1dab1979e07216f9a639f1
Kolbjørn Fredheim <hightower@tregt.net>
2008-11-28 10:43:28 +0100

    Makefile: Add pwancmdlineparser to the sources

--- src/pwancmdlineparser.h
+++ src/pwancmdlineparser.h
@@ -1,6 +1,8 @@
 #ifndef PWANCMDLINEPARSER_H
 #define PWANCMDLINEPARSER_H
 
+#include <string>
+
 namespace pwan
 {
     enum e_clpFlag { NO_PARAMETER = 0, ANY_PARAMETER = 1, DEFAULT_PARAMETER = 2 };
