a578119546b5afc6b6c7f13ff1609cdadb3efc55
Kolbjørn Fredheim <hightower@tregt.net>
2009-06-19 01:42:56 +0200

    Added a stripped down config.h to please vc++

---
+++ config.h
@@ -0,0 +1,20 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "pwantools"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "pwantools"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "pwantools 0.1.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "pwantools"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.1.0"
+
+/* Version number of package */
+#define VERSION "0.1.0"
