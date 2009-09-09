45c67037d2b8d3550655117bf764de85e752224a
hightower <hightower@hightower.cool.lan>
2009-09-09 11:32:07 +0200

    teststrings: Add ifndef/define to the headerfile

--- src/teststrings.h
+++ src/teststrings.h
@@ -1,5 +1,10 @@
+#ifndef TEST_STRINGS_H
+#define TEST_STRINGS_H
+
 void testStringFromInt(void);
 void testStringExplode(void);
 void testStringToLower(void);
 void testStringBase64Encode(void);
 void testStringDupeChar(void);
+
+#endif
